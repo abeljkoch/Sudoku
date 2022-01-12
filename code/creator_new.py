09ee15c6b784bce280aa383942ab3d00c5329718
abeljkoch <abeljkoch@gmail.com>
2022-01-12 16:54:21 +0100

    Found the problem with backtracking: it only goes one step back and then explores a new branch, but it forgets which branch already is explored..

--- code/creator_new.py
+++ code/creator_new.py
@@ -75,54 +75,6 @@ def creator():
         print(assigned_number)
 
 
-        # print(assigned_number)
-
-
-
-
-    # while np.sum(sudoku) < 405:
-    #     # get the indexes of all possible options in the truth matrix
-    #     indexes_truths = np.where(truth_matrix == 1)
-    #
-    #     # zip indexes into coordinates
-    #     coordinates = list(zip(indexes_truths[0],  indexes_truths[1], indexes_truths[2]))
-    #     # remove temp not to choose
-    #     coordinates = [cor for cor in coordinates if cor not in temp_not_to_choose]
-    #
-    #     if len(coordinates) == 0:
-    #         print(assigned_coordinates)
-    #         last_coordinate = assigned_coordinates[-1]
-    #         temp_not_to_choose = [last_coordinate]
-    #         assigned_coordinates = assigned_coordinates[0:-1]
-    #         sudoku[last_coordinate[1], last_coordinate[2]] = 0
-    #         truth_matrix = initiate_truth_matrix(sudoku)
-    #         continue
-    #
-    #     # choose random coordinate
-    #     cor = random.choice(coordinates)
-    #     # print("len coordinates: ", len(coordinates))
-    #     # print(cor)
-    #     frame_i = cor[0]
-    #     row_i = cor[1]
-    #     col_i = cor[2]
-    #
-    #     # assign random coordinate to sudoku
-    #     sudoku[row_i, col_i] = frame_i + 1
-    #     # update truth matrix for random coordinate
-    #     truth_matrix = update_locally(frame_i, row_i, col_i, truth_matrix)
-    #     truth_matrix = update_for_squares(truth_matrix)
-    #
-    #     # check if assigned number is legal
-    #     check = better_check(sudoku, truth_matrix)
-    #     if check:
-    #         sudoku[row_i, col_i] = 0
-    #         truth_matrix = initiate_truth_matrix(sudoku)
-    #         temp_not_to_choose.append(cor)
-    #     else:
-    #         assigned_coordinates.append(cor)
-    #         temp_not_to_choose = []
-    #         print(sudoku)
-
 
 if __name__ == "__main__":
     creator()
